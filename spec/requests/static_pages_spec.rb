ab578019e87dce6f1eeffb86efb4669627c5ca1e
tate <tate@PCI-BIL.site>
2015-08-17 17:42:17 +0900

    Bootstrap

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,10 +7,15 @@ RSpec.describe "StaticPages", type: :request do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
-it "should have the right title" do
-  visit '/static_pages/home'
-  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-end
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
   end
 
   describe "Help page" do
