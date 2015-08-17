6c24b85e9d7fe0d2655b655f04d69cb1441ea9fa
tate <tate@PCI-BIL.site>
2015-08-17 13:15:13 +0900

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe "StaticPages", type: :request do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+it "should have the right title" do
+  visit '/static_pages/home'
+  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+end
   end
 
   describe "Help page" do
